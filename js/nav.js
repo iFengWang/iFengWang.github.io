fb66530b085ab41a64512848ae64bf0046fdb7a0
frank.w <guangfeng.w@gmail.com>
2024-05-09 22:23:55 +0900

    edit home page

--- js/nav.js
+++ js/nav.js
@@ -3,10 +3,17 @@ var items = document.querySelectorAll('.menuItem');
 
 for(var i = 0, l = items.length; i < l; i++) {
   items[i].style.left = (50 - 35*Math.cos(-0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
-  
   items[i].style.top = (50 + 35*Math.sin(-0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
 }
 
+window.onload = function(e) {
+  setTimeout(() => {
+    e.preventDefault(); 
+    document.querySelector('.circle').classList.toggle('open');
+  }, 3000);
+}
+
 document.querySelector('.center').onclick = function(e) {
-   e.preventDefault(); document.querySelector('.circle').classList.toggle('open');
+  e.preventDefault(); 
+  document.querySelector('.circle').classList.toggle('open');
 }
