753ced9da4814c54bd3e2928fb214c0ac84bd33d
frank.w <guangfeng.w@gmail.com>
2024-08-08 10:38:42 +0900

    add homepage music

--- js/nav.js
+++ js/nav.js
@@ -1,17 +1,23 @@
 // JavaScript Document
-var items = document.querySelectorAll('.menuItem');
+var items = document.querySelectorAll(".menuItem");
 
-for(var i = 0, l = items.length; i < l; i++) {
-  items[i].style.left = (50 - 35*Math.cos(-0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
-  items[i].style.top = (50 + 35*Math.sin(-0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
+for (var i = 0, l = items.length; i < l; i++) {
+  items[i].style.left =
+    (50 - 35 * Math.cos(-0.5 * Math.PI - 2 * (1 / l) * i * Math.PI)).toFixed(
+      4
+    ) + "%";
+  items[i].style.top =
+    (50 + 35 * Math.sin(-0.5 * Math.PI - 2 * (1 / l) * i * Math.PI)).toFixed(
+      4
+    ) + "%";
 }
 
 setTimeout(() => {
-  // e.preventDefault(); 
-  document.querySelector('.circle').classList.toggle('open');
+  // e.preventDefault();
+  document.querySelector(".circle").classList.toggle("open");
 }, 3000);
 
-document.querySelector('.center').onclick = function(e) {
-  e.preventDefault(); 
-  document.querySelector('.circle').classList.toggle('open');
-}
+document.querySelector(".center").onclick = function (e) {
+  e.preventDefault();
+  document.querySelector(".circle").classList.toggle("open");
+};
